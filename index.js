4feab80fe5b8c21950abf97750bc9490e2cd7208
Nicole Ticknor <ticknor.nicole@gmail.com>
2020-04-14 02:15:15 +0000

    exercise complete

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  // base case is simple reduce
+  const result = array.reduce((acc, item) => {
+    if (Array.isArray(item)) {
+      return acc + sumItems(item);
+    }
+    return acc + item;
+  }, 0);
+  return result;
+
 }
 
 module.exports = sumItems;
